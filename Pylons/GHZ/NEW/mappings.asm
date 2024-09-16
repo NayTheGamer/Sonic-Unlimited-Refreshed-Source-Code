627f28c75ae6e0132943f5aa5cd751d6935e4d67
NayTheGamer <naylenrodgers1@gmail.com>
2024-09-16 12:15:42 +0100

    Add files via upload

---
+++ Pylons/GHZ/NEW/mappings.asm
@@ -0,0 +1,14 @@
+GHZ_Pylon:
+GHZ_Pylon_0: 	dc.w GHZ_Pylon_2-GHZ_Pylon
+GHZ_Pylon_2: 	dc.b $A
+	dc.b $24, $F, $0, $0, $F0
+	dc.b $24, $0, $0, $10, $10
+	dc.b $2C, $B, $0, $11, $D8
+	dc.b $34, $3, $0, $1D, $D0
+	dc.b $3C, $3, $0, $21, $10
+	dc.b $44, $F, $0, $25, $F0
+	dc.b $44, $B, $0, $35, $18
+	dc.b $4C, $9, $0, $41, $D8
+	dc.b $5C, $0, $0, $47, $E0
+	dc.b $5C, $0, $0, $48, $10
+	even
